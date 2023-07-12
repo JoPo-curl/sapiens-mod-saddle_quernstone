2d88412d127e0ae28e8ec31a109b35afd0f5f4f9
JoPo-curl <johan.porath@live.com>
2023-07-12 17:31:11 +0200

    all the stuff

---
+++ localizations/en_us/localizations.lua
@@ -0,0 +1,11 @@
+local localizations = {}
+
+localizations.values = {
+    -- OBJECTS
+    object_saddle_quern = "Saddle Quern",
+    object_saddle_quern_plural = "Saddle Querns",
+    object_saddle_quern_summary = "A quernstone with higher durability",
+
+}
+
+return localizations
---
+++ modInfo.lua
@@ -0,0 +1,11 @@
+local modInfo = {
+    name = "Saddle Quern",
+    description = "A costlier quernstone to craft that lasts longer",
+    version = "1.0.0",
+    type = "world",
+    developer = "Sir.Cumference",
+    loadOrder = 1,
+    preview = "assets/preview.jpg"
+}
+
+return modInfo
